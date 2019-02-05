e76f4c4bd2ee1f797cd1601824367ecb1cf22153
Pep <Pep@192.168.1.152>
2019-02-05 23:50:24 +0100

    CREADA LA CLASSE TEST PARA NUTRITION PROPERTIES
     Y MEJORAR LA COBERTURA DE LOS TESTS

---
+++ basicFood/src/basicFood/tests/NutritionPropertiesTest.java
@@ -0,0 +1,209 @@
+package basicFood.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NutritionPropertiesTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testNutritionProperties() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNutritionPropertiesStringDoubleDoubleDoubleDoubleDoubleDoubleDoubleDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAllButName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcKcal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKcal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetKcal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSatfat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSatfat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSugar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSugar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProtein() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProtein() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSalt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSalt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFiber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFiber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToStringOnlyValues() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
